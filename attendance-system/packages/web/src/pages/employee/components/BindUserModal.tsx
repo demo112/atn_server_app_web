42b4d2cb10ebb2cdbd880e4bbfd4d42aa5c6717b
songchong08204 <1060392743@qq.com>
2026-02-02 19:40:08 +0800

    fix(web): 修复组件导入路径错误

--- attendance-system/packages/web/src/pages/employee/components/BindUserModal.tsx
+++ attendance-system/packages/web/src/pages/employee/components/BindUserModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Modal, Select, message } from 'antd';
-import { getUsers } from '../../../../api/user';
+import { getUsers } from "../../../api/user";
 
 interface BindUserModalProps {
   open: boolean;
