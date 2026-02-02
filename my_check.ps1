565c27db80e377bd84e647fd27a902d5bd451035
songchong08204 <1060392743@qq.com>
2026-02-02 14:52:11 +0800

    chore: cleanup temporary debug scripts

--- my_check.ps1
+++
@@ -1,4 +0,0 @@
-Write-Output "Starting check..." | Out-File -FilePath debug.log -Encoding utf8
-Get-Command git | Out-File -FilePath git_cmd.log -Encoding utf8
-git status 2>&1 | Out-File -FilePath git_status.log -Encoding utf8
-Write-Output "Done." | Out-File -FilePath done.log -Encoding utf8
